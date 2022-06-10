e7ba30a284241c79c8ca495aa2df61d3ad693754
Your name <you@example.com>
2022-06-09 19:36:12 -0700

    up to date

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,8 +1,8 @@
+#include <stdio.h>
 /**
 * main - causes an infinite loop
 * Return: 0
 */
-
 int main(void)
 {
 	int i;
@@ -10,13 +10,13 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-
-	while (i < 10)
-	{
-		putchar(i);
-	}
-
+	/*
+	*while (i < 10)
+	*{
+	*Infinite loop - No increase of variable
+	*putchar(i);
+	*}
+	*/
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
