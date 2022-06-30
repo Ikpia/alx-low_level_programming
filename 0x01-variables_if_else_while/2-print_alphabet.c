ada0ede84e8354d7d6386e6069eb4b2fbf0bc510
Your name <you@example.com>
2022-06-30 04:38:38 -0700

    commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - funtion that prints lower case alpphabets
+* Return: 0
+*/
+
+	int main(void)
+{
+	char n = 'a';
+
+	for (n = 'a'; n <= 'z'; n++)
+{
+		putchar(n);
+}
+	return (0);
+}
