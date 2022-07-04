1384e1b7bb6e197d9e4e28dbaf3929e35f5da346
Your name <you@example.com>
2022-07-04 09:02:43 -0700

    commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,16 +11,14 @@
 
 	for (n = '0'; n <= '9'; n++)
 {
-		if (n < '9')
+	putchar(n);
+	if (n == '9')
 {
-			putchar(n);
-			putchar(',');
-			putchar(' ');
+		putchar('\n');
+		break;
 }
-		else if (n == '9')
-{
-			putchar(n);
-}
+	putchar(',');
+	putchar(' ');
 }
 	return (0);
 }
