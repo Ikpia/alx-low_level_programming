bceabcb3fe1762d3341aab7fe00d4476de858e60
Your name <you@example.com>
2022-07-07 03:50:28 -0700

    commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* print_last_digit - function that prints the last digit of a number
+* @n: integer variable
+* Return: last digit
+*/
+
+	int print_last_digit(int n)
+{
+	return (n % 10);
+}
